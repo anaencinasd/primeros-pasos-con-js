f9c47e44a5074ce1557d33728b57840d6cfb8507
anaencinasd <ana.encinasdiaz>
2023-05-22 15:13:11 +0200

    lunes mañana

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
+//Variables
+
+let nombre='Ana'
+console.log(nombre);
+
+let ciudad= 'Cádiz';
+console.log(ciudad)
+
+let andaluza = true;
+console.log(andaluza)
+
+const BirthYear = 1987;
+console.log(BirthYear);
+
+let CurrentYear = 2023
+console.log (CurrentYear);
+
+const BirthPlace = 'Córdoba'
+console.log (BirthPlace);
+
+let LivingPlace = 'Ferrol'
+console.log (LivingPlace);
+
+const DaysYear = 365;
+console.log(DaysYear)
+const MonthsYear = 12;
+console.log(MonthsYear)
+const DaysMonth = 30;
+console.log(DaysMonth)
+
+
+
+
+
+
+
 
